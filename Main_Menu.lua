fe5ca4f6e0462d2f12ddb04315bd9dc12d84a665
phucdzaivl <ngohoangphuc10112013@gmail.com>
2026-02-24 15:32:07 +0700

    Main_Menu.lua

---
+++ Main_Menu.lua
@@ -0,0 +1,6 @@
+-- this script created at https://hoannhatz.github.io/ezscript 
+getgenv().ScriptTitle = "Phucdzai Hub" 
+getgenv().ScriptSubTitle = "Version 1.0" 
+getgenv().ScriptImage = "https://i.ibb.co/mCzjBC5y/phucdzai.jpg" 
+getgenv().ScriptAuthorName = "By Phuc Ngo" 
+getgenv().ScriptAuthorSubTitle = "https://discord.gg/q8jMMgKx" loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
