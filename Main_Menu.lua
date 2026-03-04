c5c44610c13aa9e9d01053f8453589e1a81a3372
phucdzaivl <ngohoangphuc10112013@gmail.com>
2026-03-04 12:44:38 +0700

    Update Main_Menu.lua

--- Main_Menu.lua
+++ Main_Menu.lua
@@ -3,4 +3,5 @@ getgenv().ScriptTitle = "Phucdzai Hub"
 getgenv().ScriptSubTitle = "Version 1.0" 
 getgenv().ScriptImage = "https://i.ibb.co/mCzjBC5y/phucdzai.jpg" 
 getgenv().ScriptAuthorName = "By Phuc Ngo" 
-getgenv().ScriptAuthorSubTitle = "https://discord.gg/q8jMMgKx" loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
+getgenv().ScriptAuthorSubTitle = "https://discord.gg/q8jMMgKx" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
